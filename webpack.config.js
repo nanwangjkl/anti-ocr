5e2be7ebd30236722f0975bd0c745236a458d7c2
Nan, Wang <552075677@qq.com>
2022-03-01 22:27:05 +0800

    更新webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ]
+};
