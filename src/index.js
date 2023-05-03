89c869d5b8a7466a00406b51ba3777c74965b451
HeavyPanzer <552075677@qq.com>
2023-05-04 02:00:49 +0800

    Reverse img (#2)
    * 更新index.html
    
    * 更新效果
    
    * 更新文字蒙板格式
    
    * 更新三个预设
    
    * 更新图片，蒙板
    
    * 更新preset
    
    * 文字滤镜

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './index.css'
 import * as PIXI from 'pixi.js-legacy'
-import bannerSolution from './solutions/banner'
+import reverseSolution from './solutions/reverse-img'
 
 const form = document.querySelector('#form')
 const canvasDiv = document.querySelector('.target-canvas')
-const heightTemplate = document.querySelector('.height-template>pre')
+const generateBtn = document.querySelector('#generate')
 const downloadBtn = document.querySelector('#download')
 const mainContent = document.querySelector('#main')
 const loadingNotice = document.querySelector('#loading-notice')
@@ -19,15 +19,14 @@ form.addEventListener('submit', event => {
   event.preventDefault()
   const formData = new window.FormData(form)
   const text = formData.get('text')
-  const color = formData.get('color')
-  const strength = formData.get('strength')
-  // 使用一个透明的pre决定画布高度
-  heightTemplate.innerHTML = text
-  const height = heightTemplate.offsetHeight
-  canvasDiv.style.height = `${height}px`
-  app.stage.removeChildren()
-  app.resize()
-  bannerSolution(app, { text, color, strength, fontSize: FONT_SIZE, lineHeight: LINE_HEIGHT })
+  const fontSize = Number(formData.get('fontSize'))
+  const lineHeight = Number(formData.get('lineHeight'))
+  const font = formData.get('font')
+  const maskFormat = formData.get('maskFormat')
+  const borderFormat = formData.get('borderFormat')
+  const preset = formData.get('preset')
+  // 生成图片
+  reverseSolution(app, { text, img: preset, fontSize, lineHeight, font, maskFormat, borderFormat })
   downloadBtn.disabled = false
 })
 
@@ -45,6 +44,11 @@ downloadBtn.onclick = event => {
 document.fonts.ready.then(() => {
   mainContent.hidden = false
   loadingNotice.hidden = true
+  // 添加img元素以决定画布高度
+  const img = document.createElement('img')
+  img.src = '/img/01.jpg'
+  canvasDiv.appendChild(img)
+  // 决定画布高度
   width = canvasDiv.offsetWidth
   app = new PIXI.Application({
     width,
@@ -55,3 +59,7 @@ document.fonts.ready.then(() => {
   })
   canvasDiv.appendChild(app.view)
 })
+
+setTimeout(() => {
+  generateBtn.click()
+}, 100)
