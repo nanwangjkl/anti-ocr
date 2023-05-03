b2b6746ee104f9485bcfce03ccf5491475358ebd
nanwangjkl <552075677@qq.com>
2023-05-04 00:01:50 +0800

    更新三个预设

--- src/preset.js
+++ src/preset.js
@@ -2,5 +2,13 @@ export default {
   '01': {
     img: '/img/01.jpg',
     borderColor: '#171102'
+  },
+  '02': {
+    img: '/img/02.jpg',
+    borderColor: '#e7af75'
+  },
+  '03': {
+    img: '/img/03.jpg',
+    borderColor: '#171102'
   }
 }
